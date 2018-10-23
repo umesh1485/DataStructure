9ae217edcf4684cc58bf090b9888a1687276f798
C902179 <umesh.chander@merrillcorp.com>
2018-10-23 17:54:36 +0530

    Reverse method implemented.

--- src/com/singlyLnkList/operations/LinkedList.java
+++ src/com/singlyLnkList/operations/LinkedList.java
@@ -30,52 +30,69 @@ public class LinkedList {
 		newNode.data = data;
 		int counter = 0;
 		Node h = head;
-		
-		if(index==0){
+
+		if (index == 0) {
 			Node bck = head;
 			newNode.next = bck;
 			head = newNode;
-		}else{	
+		} else {
 			while (h != null) {
-				if(index == ++counter){
+				if (index == ++counter) {
 					Node bck = h.next;
-					h.next = newNode; 
+					h.next = newNode;
 					newNode.next = bck;
 				}
 				h = h.next;
 			}
 		}
+		size++;
 	}
-	
+
 	public void remove(int index) {
 		if (index >= size) {
 			throw new ArrayIndexOutOfBoundsException("Invalid Index");
 		}
-		
+
 		int counter = 0;
-		if(index==0){
+		if (index == 0) {
 			Node bck = head.next;
 			head = bck;
-		}else{	
+		} else {
 			Node h = head;
 			while (h != null) {
-				if(index == ++counter){
+				if (index == ++counter) {
 					Node bck = h.next.next;
-					 h.next = bck;
+					h.next = bck;
 				}
 				h = h.next;
 			}
 		}
 	}
 
-	public void show() {
-		Node n = head;
-		while (n != null) {
-			System.out.print("  "+n.data);
-			n = n.next;
+	public Node reverse(Node head) {
+		Node next = null;
+		Node prev = null;
+		Node current = head;
+		
+		while(current!=null){
+			next = current.next;
+			current.next = prev;
+			prev = current;
+			current = next;
 		}
-		System.out.println("");
+		head = prev;
+		return head;
 	}
+	
+
+	// prints content of double linked list 
+    void printList(Node node) { 
+        while (node != null) { 
+            System.out.print(node.data + " "); 
+            node = node.next; 
+        } 
+        System.out.println("");
+    }
 
 	class Node {
 		int data;
--- src/com/singlyLnkList/operations/Runner.java
+++ src/com/singlyLnkList/operations/Runner.java
@@ -4,19 +4,29 @@ public class Runner {
 
 	public static void main(String[] args) {
 		LinkedList list = new LinkedList();
+		
 		//insert
 		list.insert(12);
 		list.insert(14);
 		list.insert(15);
-		list.show();
+		//Read
+		System.out.println("Read: ");
+		list.printList(list.head);
+		
 		//Update
 		list.insert(2, 444);
-		//Read
-		list.show();
+		System.out.println("Update: list.insert(2, 444)");
+		list.printList(list.head);
+		
 		//Delete
 		list.remove(0);
+		System.out.println("Delete: remove(0)");
+		list.printList(list.head);
 		
-		list.show();
+		//reverse
+		LinkedList.Node node =list.reverse(list.head);
+		System.out.println("Reverse:");
+		list.printList(node);
 		
 		
 	}
